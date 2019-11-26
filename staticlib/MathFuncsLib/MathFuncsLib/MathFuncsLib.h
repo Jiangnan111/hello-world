c961f86bd6a3ca8ddd91ac998711511c7e62d802
Jiangnan11111 <zzjn719@126.com>
2019-11-26 09:27:04 +0800

    静态库实现与调用

---
+++ staticlib/MathFuncsLib/MathFuncsLib/MathFuncsLib.h
@@ -0,0 +1,14 @@
+#pragma once
+namespace MathFuncs
+{
+	class MyMathFuncs {
+	public:
+		static double Add(double a, double b);
+
+		static double Subtract(double a,double b);
+
+		static double Multiply(double a, double b);
+
+		static double Divide(double a, double b);
+	};
+}
